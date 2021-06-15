c4cfe4c4f1adb34483bbb18ac5c5b0d1f87d34de
Artsiom Pratsenka <protsenko@wimix.by>
2021-06-15 13:11:08 +0300

    added printx2

--- push_notifications/gcm.py
+++ push_notifications/gcm.py
@@ -150,6 +150,7 @@ def _cm_send_request(
 
 	# Sort the keys for deterministic output (useful for tests)
 	json_payload = json.dumps(payload, separators=(",", ":"), sort_keys=True).encode("utf-8")
+	print('json_payload is ', json_payload)
 
 	# Sends requests and handles the response
 	if cloud_type == "GCM":
